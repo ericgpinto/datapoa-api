b230222b356343ef05b893c6369453980cd4cbe8
Éric Pinto <ericgrandopinto@gmail.com>
2021-03-15 23:47:19 -0300

    first commit

---
+++ src/main/java/com/ericpinto/datapoa/DatapoaApplication.java
@@ -0,0 +1,13 @@
+package com.ericpinto.datapoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatapoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericpinto/datapoa/DatapoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericpinto.datapoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatapoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
